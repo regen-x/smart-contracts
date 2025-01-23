3fb145493fe1ef1e0482be17c8689ed3fcf02e58
gabicasas01 <casas.gf7@gmail.com>
2025-01-23 14:46:25 -0300

    Update module for public implementations

--- src/methods/public/initialize.rs
+++ src/methods/public/initialize.rs
@@ -3,18 +3,18 @@ use soroban_sdk::{Address, Env};
 use crate::{
     events,
     storage::{
-        admin::{has_admin, set_admin},
-        types::error::Error,
+        admin::set_admin,
+        reference_token::set_reference_token,
+        types::{error::Error, reference_token::ReferenceToken},
     },
 };
 
-pub fn initialize(env: &Env, admin: Address) -> Result<(), Error> {
-    if has_admin(env) {
-        return Err(Error::ContractInitialized);
-    }
+pub fn initialize(env: &Env, admin: Address, reference_token: Address) -> Result<(), Error> {
+    admin.require_auth();
+    
+    set_admin(env, &admin);
+    set_reference_token(env, &ReferenceToken { address: reference_token });
 
-    set_admin(&env, &admin);
-    events::contract::contract_initialized(&env, &admin);
-
+    events::contract::contract_initialized(env, &admin);
     Ok(())
 }
--- src/methods/public/transfer.rs
+++ src/methods/public/transfer.rs
@@ -1,11 +1,37 @@
 use soroban_sdk::{token, Address, Env};
+use crate::storage::{reference_token::get_reference_token, token::read_token, types::error::Error, admin::get_admin};
 
-pub fn transfer(env: &Env, from: Address, to: Address, token: Address, amount: i128) -> i128 {
-    from.require_auth();
+pub fn transfer(
+    env: &Env,
+    owner: Address,
+    investor: Address,
+    token_address: Address,
+    amount: i128,
+) -> Result<(), Error> {
+    let admin = get_admin(env);
 
-    let client = token::Client::new(env, &token);
+    admin.require_auth();
+    investor.require_auth();
 
-    client.transfer(&from, &to, &amount);
+    let token = read_token(env, &token_address);
+    let reference_token = get_reference_token(env);
+    let reference_amount = amount * token.price;
 
-    client.balance(&to)
+    let reference_client = token::Client::new(env, &reference_token.address);
+
+    if reference_client.balance(&investor) < reference_amount {
+        return Err(Error::InsufficientBalance);
+    }
+
+    reference_client.transfer(&investor, &owner, &reference_amount);
+
+    let token_client = token::Client::new(env, &token_address);
+
+    if token_client.balance(&admin) < amount {
+        return Err(Error::InsufficientBalance);
+    }
+
+    token_client.transfer(&admin, &investor, &amount);
+
+    Ok(())
 }
