804c912c14a7482790405e6f22c90bdcf568cd0c
gabicasas01 <casas.gf7@gmail.com>
2025-01-23 14:48:07 -0300

    Update Contract to implement new functionalities

--- src/contract.rs
+++ src/contract.rs
@@ -2,11 +2,11 @@ use soroban_sdk::{contract, contractimpl, Address, Env};
 
 use crate::{
     methods::{
-        admin::set_admin::set_admin,
+        admin::{get_admin::get_admin, has_admin::has_admin, set_admin::set_admin},
         public::{initialize::initialize, transfer::transfer},
-        user::user::{get_user, update_transfer_amount},
+        token::{set_reference_token::set_reference_token, write_token::write_token},
     },
-    storage::types::{error::Error, user::User},
+    storage::types::{error::Error, reference_token::ReferenceToken, token::Token},
 };
 
 #[contract]
@@ -14,21 +14,48 @@ pub struct Contract;
 
 #[contractimpl]
 impl Contract {
-    pub fn initialize(env: Env, admin: Address) -> Result<(), Error> {
-        initialize(&env, admin)
+    pub fn __constructor(env: Env, admin: Address, reference_token: Address) -> Result<(), Error> {
+        initialize(&env, admin, reference_token)
     }
 
     pub fn set_admin(env: Env, admin: Address) -> Result<Address, Error> {
         set_admin(&env, admin)
     }
 
-    pub fn transfer(env: Env, from: Address, to: Address, token: Address, amount: i128) -> i128 {
-        update_transfer_amount(&env, &from);
-
-        transfer(&env, from, to, token, amount)
+    pub fn set_reference_token(env: Env, token_address: Address) -> Result<ReferenceToken, Error> {
+        set_reference_token(
+            &env,
+            &ReferenceToken {
+                address: token_address,
+            },
+        )
     }
 
-    pub fn get_user(env: Env, address: Address) -> Result<User, Error> {
-        get_user(&env, &address)
+    pub fn write_token(env: Env, token: Address, price: i128) -> Result<Token, Error> {
+        write_token(
+            &env,
+            &Token {
+                address: token,
+                price,
+            },
+        )
+    }
+
+    pub fn transfer(
+        env: Env,
+        owner: Address,
+        investor: Address,
+        token_address: Address,
+        amount: i128,
+    ) -> Result<(), Error> {
+        transfer(&env, owner, investor, token_address, amount)
+    }
+
+    pub fn has_admin(env: Env) -> bool {
+        has_admin(&env)
+    }
+
+    pub fn get_admin(env: Env) -> Address {
+        get_admin(&env)
     }
 }
--- src/tests/contract.rs
+++ src/tests/contract.rs
@@ -1,59 +1,63 @@
+use crate::tests::config::contract::ContractTest;
+use crate::{contract::ContractClient, tests::config::constants::BASE_MINT_AMOUNT, Contract};
 use soroban_sdk::testutils::Events;
-
-use crate::tests::config::{constants::BASE_MINT_AMOUNT, contract::ContractTest};
+use soroban_sdk::{testutils::Address as _, Address, Env};
 
 #[test]
 fn initialize_test() {
-    let ContractTest {
-        env,
-        contract,
-        admin,
-        ..
-    } = ContractTest::setup();
+    let env = Env::default();
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    let reference_token_issuer = Address::generate(&env);
 
-    let initial_events = env.events().all().clone();
-    assert_eq!(initial_events.len(), 3);
+    let initial_events: soroban_sdk::Vec<(
+        Address,
+        soroban_sdk::Vec<soroban_sdk::Val>,
+        soroban_sdk::Val,
+    )> = env.events().all().clone();
+    assert_eq!(initial_events.len(), 0);
 
-    contract.initialize(&admin);
+    let contract_id = env.register(Contract, (&admin, &reference_token_issuer));
+    let contract = ContractClient::new(&env, &contract_id);
 
-    assert_eq!(env.events().all().len(), initial_events.len() + 1);
+    assert!(contract.has_admin());
+    assert_eq!(contract.get_admin(), admin);
 }
 
 #[test]
-#[should_panic = "Error(Contract, #0)"]
+#[should_panic(expected = "Error(Value, InvalidInput)")]
 fn initialize_fail_test() {
-    let ContractTest {
-        contract,
-        admin,
-        user_a,
-        ..
-    } = ContractTest::setup();
+    let env = Env::default();
+    env.mock_all_auths();
 
-    contract.initialize(&admin);
-    contract.initialize(&user_a);
+    let invalid_admin = "TEST";
+    let reference_token_issuer = Address::generate(&env);
+
+    env.register(
+        Contract,
+        (
+            Address::from_str(&env, &invalid_admin),
+            &reference_token_issuer,
+        ),
+    );
 }
 
 #[test]
 fn set_admin_test() {
     let ContractTest {
-        contract,
-        admin,
-        user_a,
-        ..
-    } = ContractTest::setup();
-
-    contract.initialize(&admin);
-    contract.set_admin(&user_a);
-}
-
-#[test]
-#[should_panic = "Error(Contract, #1)"]
-fn set_admin_fail_test() {
-    let ContractTest {
         contract, user_a, ..
     } = ContractTest::setup();
 
     contract.set_admin(&user_a);
+}
+
+#[test]
+fn set_reference_token_test() {
+    let ContractTest {
+        contract, user_a, ..
+    } = ContractTest::setup();
+
+    contract.set_reference_token(&user_a);
 }
 
 #[test]
@@ -62,59 +66,64 @@ fn transfer_test() {
         contract,
         user_a,
         user_b,
+        admin,
         token,
+        reference_token,
+        env,
         ..
     } = ContractTest::setup();
+    env.mock_all_auths();
 
     let (token_client, _, __) = token;
+    let (reference_token_client, _, __) = reference_token;
 
-    let transfer_amount = BASE_MINT_AMOUNT / 2;
+    let token_price = 2;
 
-    assert_eq!(token_client.balance(&user_a), BASE_MINT_AMOUNT);
-    assert_eq!(token_client.balance(&user_b), BASE_MINT_AMOUNT);
+    contract.write_token(&token_client.address, &token_price);
 
-    contract.transfer(&user_a, &user_b, &token_client.address, &transfer_amount);
+    let transfer_amount = 2;
+
+    assert_eq!(reference_token_client.balance(&user_a), BASE_MINT_AMOUNT);
+    assert_eq!(token_client.balance(&admin), BASE_MINT_AMOUNT);
+
+    contract.transfer(&user_b, &user_a, &token_client.address, &transfer_amount);
 
     assert_eq!(
-        token_client.balance(&user_a),
+        token_client.balance(&admin),
         BASE_MINT_AMOUNT - transfer_amount
     );
     assert_eq!(
-        token_client.balance(&user_b),
-        BASE_MINT_AMOUNT + transfer_amount
+        reference_token_client.balance(&user_b),
+        transfer_amount * token_price
     );
 }
 
 #[test]
-fn get_user_test() {
+#[should_panic(expected = "Error(Contract, #2)")]
+fn transfer_insufficient_balance_investor() {
     let ContractTest {
         contract,
         user_a,
         user_b,
+        admin,
         token,
+        reference_token,
+        env,
         ..
     } = ContractTest::setup();
+    env.mock_all_auths();
 
     let (token_client, _, __) = token;
+    let (reference_token_client, _, __) = reference_token;
 
-    let transfer_amount = BASE_MINT_AMOUNT / 2;
+    let token_price = 2;
 
-    contract.transfer(&user_a, &user_b, &token_client.address, &transfer_amount);
+    contract.write_token(&token_client.address, &token_price);
 
-    let user_a_historical_data = contract.get_user(&user_a);
-    assert_eq!(user_a_historical_data.transactions_count, 1);
-    contract.transfer(&user_a, &user_b, &token_client.address, &transfer_amount);
+    let transfer_amount = BASE_MINT_AMOUNT + 1;
 
-    let user_a_historical_data_2 = contract.get_user(&user_a);
-    assert_eq!(user_a_historical_data_2.transactions_count, 2);
-}
+    assert_eq!(reference_token_client.balance(&user_a), BASE_MINT_AMOUNT);
+    assert_eq!(token_client.balance(&admin), BASE_MINT_AMOUNT);
 
-#[test]
-#[should_panic = "Error(Contract, #2)"]
-fn get_user_fail_test() {
-    let ContractTest {
-        contract, user_a, ..
-    } = ContractTest::setup();
-
-    contract.get_user(&user_a);
+    contract.transfer(&user_b, &user_a, &token_client.address, &transfer_amount);
 }
