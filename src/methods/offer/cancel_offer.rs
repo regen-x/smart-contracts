f0fa9c19c113b5f873cb93afb1585e1230b6c945
ElectroCERP <carloscer27r@gmail.com>
2025-02-13 14:05:27 -0300

    Verify is an offer is active before updating or canceling it

--- src/methods/offer/cancel_offer.rs
+++ src/methods/offer/cancel_offer.rs
@@ -2,13 +2,17 @@ use soroban_sdk::{token, Env};
 use crate::storage::{offer::{cancel_offer as cancel_offer_storage, read_offer}, types::{error::Error, offer::Offer}};
 
 pub(crate) fn cancel_offer(env: &Env, offer_id: &i128) -> Result<(i128, Offer), Error> {
-    let offer = read_offer(env, offer_id);
+    let (_, offer) = read_offer(env, offer_id);
 
-    let owner = offer.1.owner;
+    if !offer.is_active {
+        return Err(Error::OfferNotActive);
+    }
+
+    let owner = offer.owner;
     owner.require_auth();
 
-    let token_client = token::Client::new(env, &offer.1.token_address);
-    token_client.transfer(&env.current_contract_address(), &owner, &offer.1.amount);
+    let token_client = token::Client::new(env, &offer.token_address);
+    token_client.transfer(&env.current_contract_address(), &owner, &offer.amount);
     
     cancel_offer_storage(env, offer_id);
 
--- src/methods/offer/update_offer_price.rs
+++ src/methods/offer/update_offer_price.rs
@@ -4,6 +4,10 @@ use crate::storage::{offer::{read_offer, update_offer}, types::{error::Error, of
 pub(crate) fn update_offer_price(env: &Env, offer_id: &i128, price: i128) -> Result<(i128, Offer), Error> {
     let (_, offer) = read_offer(env, offer_id);
 
+    if !offer.is_active {
+        return Err(Error::OfferNotActive);
+    }
+
     let owner = offer.owner;
     owner.require_auth();
    
